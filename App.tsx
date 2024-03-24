8cda43590732e1ce6787124e8ac9ab4f5f78ba6f
wesleywil <wesleywilsonti@gmail.com>
2024-03-24 09:17:33 -0300

    added the database init configuration to the App.tsx

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -6,9 +7,16 @@ import HomeScreen from "./screens/homeScreen/homeScreen";
 import PlansList from "./screens/plansList/plansList";
 import AddPlan from "./screens/addPlan";
 
+// Database
+import DatabaseInit from "./database/db-service";
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    const database = new DatabaseInit();
+    console.log("initialize database!");
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator>
