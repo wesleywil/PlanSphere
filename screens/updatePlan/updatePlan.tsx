7cca3bf11d0e8d52206d83de844881dd82ba655b
wesleywil <wesleywilsonti@gmail.com>
2024-03-27 22:03:48 -0300

    added update Plan mockup screen

---
+++ screens/updatePlan/updatePlan.tsx
@@ -0,0 +1,18 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableHighlight,
+} from "react-native";
+
+const UpdatePlan = ({ route, navigation }: any) => {
+  const { id } = route.params;
+  return (
+    <View>
+      <Text>Update Plan - plansTitle</Text>
+    </View>
+  );
+};
+
+export default UpdatePlan;
