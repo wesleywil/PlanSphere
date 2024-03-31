b6516e7af34d7b9b0043290c82733a41dd4aa6ae
wesleywil <wesleywilsonti@gmail.com>
2024-03-31 08:58:06 -0300

    updated Plan interface by adding a enum to the priority attribute

--- utils/models.ts
+++ utils/models.ts
@@ -1,7 +1,21 @@
 export interface Plan{
     id?:number,
     title:string,
-    priority?:string,
+    priority?:Priority,
     note?:string,
     limit_date?:Date
+}
+
+export enum Priority{
+    No_Priority = "No Priority",
+    High = "High",
+    Mid = "Mid",
+    Low = "Low",
+}
+
+export const PriorityColorMap:Record<Priority, string> = {
+    [Priority.No_Priority]:"white",
+    [Priority.High]:"red",
+    [Priority.Mid]:"yellow",
+    [Priority.Low]:"green",
 }
